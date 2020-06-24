c643eedac3f795bf7cd5006b1cedcdc666610777
heralt <heralt@live.nl>
2020-06-24 23:19:41 +0200

    add database connection

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const fetch = require('node-fetch');
+const levelup = require('levelup');
+const leveldown = require('leveldown');
+
+const db = levelup(leveldown('./songDb'));
 
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
@@ -15,31 +19,56 @@ const PLAYLIST_ID = '71LMuR914gr93PYJweS7lO';
 
 let queuedSongs = 0;
 
+async function getSongFromDB(songID) {
+    try {
+        let text = await db.get(songID);
+        let value = JSON.parse(text);
+        return value;
+    } catch (e) {
+        return e;
+    }
+}
+
+function putSongInDb(songID, song) {
+    db.put(songID, song, function (err) {
+        if (err) return console.log('Ooops!', err)
+    });
+}
+
+function getSongFromSpotify(songData) {
+    let url = `https://api.spotify.com/v1/tracks/${songData.songID}`;
+    return fetch(url, {
+        headers: {
+            'Authorization': 'Bearer ' + songData.access
+        }
+    }).then(response => {
+        return response.json();
+    }).then(result => {
+        return result;
+    });
+}
+
 io.on('connection', (socket) => {
 
     let {username} = socket;
+    console.log(username);
     username = "Anonymous";
 
     socket.on('other event', (data) => {
         console.log(data)
     });
 
-    socket.on('connected',() => {
-        socket.emit('playlistID',{
+    socket.on('connected', () => {
+        socket.emit('playlistID', {
             playlistID: PLAYLIST_ID
         });
     });
 
     socket.on('click song', (data) => {
-        let url = `https://api.spotify.com/v1/tracks/${data.songID}`;
-        fetch(url,{
-            headers: {
-                'Authorization': 'Bearer ' + data.access
-            }
-        }).then(response => {
-            return response.json();
-        }).then(result => {
-            io.sockets.emit('add song',{
+        let song = data.songID;
+        getSongFromSpotify(data).then(result => {
+            putSongInDb(song, JSON.stringify(result));
+            io.sockets.emit('add song', {
                 song: result,
                 playlist: PLAYLIST_ID,
             });
@@ -47,42 +76,47 @@ io.on('connection', (socket) => {
     });
 
     socket.on('click song que', (data) => {
-        console.log('que', socket.id);
-        let url = `https://api.spotify.com/v1/tracks/${data.songID}`;
-        fetch(url,{
-            headers: {
-                'Authorization': 'Bearer ' + data.access
+        let song = data.songID;
+        getSongFromDB(song).then(value => {
+            if(value.notFound){
+                console.log('from api');
+                getSongFromSpotify(data).then(result => {
+                    putSongInDb(song, JSON.stringify(result));
+                    io.emit('queue song', {
+                        song: result,
+                        playlist: PLAYLIST_ID,
+                    });
+                });
+            } else {
+                console.log('from db');
+                io.emit('queue song', {
+                    song: value,
+                    playlist: PLAYLIST_ID,
+                });
             }
-        }).then(response => {
-            return response.json();
-        }).then(result => {
-            io.emit('queue song',{
-                song: result,
-                playlist: PLAYLIST_ID
-            });
         });
     });
 
-    socket.on('queued songs',()=>{
-        queuedSongs +=1;
-        io.sockets.emit('queued songs',{
-           queued: queuedSongs
-       });
+    socket.on('queued songs', () => {
+        queuedSongs += 1;
+        io.sockets.emit('queued songs', {
+            queued: queuedSongs
+        });
     });
 
-    socket.on('queued played',()=>{
-        if(queuedSongs > 0) {
+    socket.on('queued played', () => {
+        if (queuedSongs > 0) {
             queuedSongs -= 1;
         } else {
             queuedSongs = 0;
         }
-        io.sockets.emit('queued songs',{
+        io.sockets.emit('queued songs', {
             queued: queuedSongs
         });
     });
 
     socket.on('song no active', (data) => {
-        socket.emit('change state',{
+        socket.emit('change state', {
             state: data.state
         })
     });
@@ -92,6 +126,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('change_username', (data) => {
+        console.log(username);
         username = data.username;
     });
 
